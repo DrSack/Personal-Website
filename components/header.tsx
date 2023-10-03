eb9e20326686dabef867d52020a2307e50ecf053
DrSack <travismangila1999@gmail.com>
2023-10-03 23:21:56 +1000

    added navigation to mobile header

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { AppBar, Box, Drawer, IconButton, Toolbar, Typography, useMediaQuery } from '@mui/material';
+import { AppBar, Box, Drawer, IconButton, List, ListItem, ListItemButton, ListItemText, Toolbar, Typography, useMediaQuery } from '@mui/material';
 import { TextSnippet } from '@mui/icons-material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { useRouter } from 'next/navigation';
@@ -55,53 +55,76 @@ export const Header = () => {
 
   return (
     <>
-        <StyledHeaderDesktop marginBottom={12}>
-          <AppBar>
-            <Toolbar style={{ backgroundColor: 'white', color: 'black' }}>
-              <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                sx={{ mr: 2 }}
-                onClick={() => setOpen(true)}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography component="div" sx={{ flexGrow: 1 }}>
-                Travis Mangila
-              </Typography>
-            </Toolbar>
-          </AppBar>
-          <Drawer
-            anchor={'left'}
-            open={open}
-            onClose={() => setOpen(false)}
-          >
-            This will be filled information later
-          </Drawer>
-        </StyledHeaderDesktop>
-          <StyledHeaderMobile padding={2}>
-            <Toolbar style={{ color: 'black', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-              <Box gap={2} onClick={() => router.push('/')} style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', cursor: 'pointer' }}>
-                <TextSnippet />
-                <StyledTitleTypography sx={{ flexGrow: 1 }}>
-                  Travis Mangila
-                </StyledTitleTypography>
-              </Box>
-              <Box style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-                <IconButton href='/skills' size='medium'>
-                  <StyledLinksTypography>Skills</StyledLinksTypography>
-                </IconButton>
-                <IconButton href='/work' size='medium'>
-                  <StyledLinksTypography>My Work</StyledLinksTypography>
-                </IconButton>
-                <IconButton size='medium'>
-                  <StyledLinksTypography>Contact</StyledLinksTypography>
-                </IconButton>
-              </Box>
-            </Toolbar>
-          </StyledHeaderMobile>
+      <StyledHeaderDesktop marginBottom={12}>
+        <AppBar>
+          <Toolbar style={{ backgroundColor: 'white', color: 'black' }}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              onClick={() => setOpen(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography component="div" sx={{ flexGrow: 1 }}>
+              Travis Mangila
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          anchor={'left'}
+          open={open}
+          onClose={() => setOpen(false)}
+        >
+          <Box role="presentation">
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton href='/'>
+                  <ListItemText primary='Home' />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton href='/skills'>
+                  <ListItemText primary='Skills' />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton href='/work'>
+                  <ListItemText primary='My Work' />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemText primary='Contact' />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </Box>
+        </Drawer>
+      </StyledHeaderDesktop>
+      <StyledHeaderMobile padding={2}>
+        <Toolbar style={{ color: 'black', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+          <Box gap={2} onClick={() => router.push('/')} style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', cursor: 'pointer' }}>
+            <TextSnippet />
+            <StyledTitleTypography sx={{ flexGrow: 1 }}>
+              Travis Mangila
+            </StyledTitleTypography>
+          </Box>
+          <Box style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+            <IconButton href='/skills' size='medium'>
+              <StyledLinksTypography>Skills</StyledLinksTypography>
+            </IconButton>
+            <IconButton href='/work' size='medium'>
+              <StyledLinksTypography>My Work</StyledLinksTypography>
+            </IconButton>
+            <IconButton size='medium'>
+              <StyledLinksTypography>Contact</StyledLinksTypography>
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </StyledHeaderMobile>
     </>
   )
 }
